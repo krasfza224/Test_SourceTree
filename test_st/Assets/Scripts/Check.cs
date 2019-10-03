4f9c1ad219e523f2a32fcd156b81e746679d4c2f
DESKTOP-PKG5F0N\lenovo <zalive.kar@gmail.com>
2019-10-03 20:48:25 +0700

    add scenes and cube

---
+++ test_st/Assets/Scripts/Check.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Check : MonoBehaviour       
+{
+    public int m_hp, m_mana, m_stamina;
+    public int id;
+
+   /* public int hp
+    {
+      set
+        {
+            m_hp = value;
+        }
+        get
+        {
+            return m_hp;
+        }
+    }
+
+    public  int mana
+    {
+        set
+        {
+            m_mana = value;
+        }
+        get
+        {
+            return m_mana;
+        }
+    }
+
+    public  int stamina
+    {
+        set
+        {
+            m_stamina = value;
+        }
+        get
+        {
+            return m_stamina;
+        }
+    }*/
+
+   public void SetHP(int hp)
+    {
+        hp = m_hp;
+    }
+
+   public int GetHP()
+    {
+        return m_hp;
+    }
+
+    public void SetMana(int Mana)
+    {
+        Mana = m_mana;
+    }
+
+    public int GetMana()
+    {
+        return m_mana;
+    }
+
+    public void SetStamina(int Stamina)
+    {
+        Stamina = m_stamina;
+    }
+
+    public int GetStamina()
+    {
+        return m_stamina;
+        ;
+    }
+}
